70ae23ba89b28e73daf5ae47bab76b7d9a53a901
hatamoru <hatamoru893@gmail.com>
2021-07-29 20:39:51 +0900

    first commit

---
+++ src/test/java/com/example/tagulog/TagulogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tagulog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagulogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
